a5cc6118ab326386039e146f68dc28e93598539b
wookie184 <wookie1840@gmail.com>
2022-02-13 16:28:45 +0000

    Rewrite cli to use subcommands for saving to files

--- maze/cli.py
+++ maze/cli.py
@@ -1,19 +1,18 @@
 import pathlib
-from typing import Optional
 
 import click
 
 from . import display
-from .constants import ColourInfo, MediaFormat, SaveOption, Size, Speed
+from .constants import Size
 
 
-def to_mediaformat(
-    ctx: click.Context, param: click.Parameter, value: Optional[str]
-) -> Optional[MediaFormat]:
-    if value is None:
-        return None
+def add_options(options):
+    def _add_options(func):
+        for option in reversed(options):
+            func = option(func)
+        return func
 
-    return MediaFormat.from_string(value)
+    return _add_options
 
 
 def validate_size(value: str) -> Size:
@@ -42,56 +41,49 @@ def validate_window_size(
     return size
 
 
-@click.command("Maze Generator", context_settings={"max_content_width": 110})
-@click.option(
-    "--grid-size",
-    "-g",
-    default="10x10",
-    callback=validate_grid_size,
-    help="The dimensions of the maze grid, e.g. 10x10",
-)
-@click.option(
-    "--window-size",
-    "-w",
-    default="700x600",
-    callback=validate_window_size,
-    help="The resolution of the window, e.g. 100x200",
-)
-@click.option(
-    "--colour-speed",
-    "-c",
-    default=10,
-    help="The speed the colour changes. Enter 0 for no colour change",
-)
-@click.option(
-    "--start-colour",
-    default=0,
-    help="The colour to start at, given as a HSV hue between 0 and 255",
-)
-@click.option(
-    "--frames-per-second",
-    "-fps",
-    default=20,
-    help="Number of updates per second to aim for",
-)
-@click.option(
-    "--steps-per-frame", "-spf", default=1, help="Number of steps to make per frame"
-)
-@click.option(
-    "--save-format",
-    type=click.Choice(MediaFormat.formats(), case_sensitive=False),
-    callback=to_mediaformat,
-    default=None,
-    show_default=True,
-    help=(
-        "Save the maze generation in the format passed. Nothing is saved by default. "
-        "Video formats will save an animation, while image formats save the final frame. "
-        "Using this option will run the live animation at maximum speed, and the -fps argument "
-        "will be used for the video fps"
+MAZE_OPTIONS = [
+    click.option(
+        "--window-size",
+        "-w",
+        "window_size",
+        default="700x600",
+        callback=validate_window_size,
+        help="Size of the window, e.g. 100x200",
     ),
-)
-@click.option(
+    click.option(
+        "--grid-size",
+        "-g",
+        "grid_size",
+        default="10x10",
+        callback=validate_grid_size,
+        help="Dimensions of the grid, e.g. 10x10",
+    ),
+    click.option(
+        "--colour-speed",
+        "colour_speed",
+        default=1,
+        type=click.FloatRange(0, 255),
+        help="Colour change as HSV hue per generation step",
+    ),
+    click.option(
+        "--start-colour",
+        "colour_start",
+        default=0,
+        type=click.IntRange(0, 255),
+        help="The colour to start at as a HSV hue",
+    ),
+    click.option(
+        "--step",
+        "step",
+        default=1,
+        type=click.IntRange(1),
+        help="Number of generation steps per frame",
+    ),
+]
+
+SAVE_PATH_OPTION = click.option(
     "--save-path",
+    "save_path",
     type=click.Path(
         exists=False,
         dir_okay=False,
@@ -100,27 +92,46 @@ def validate_window_size(
         path_type=pathlib.Path,
     ),
     default=pathlib.Path.cwd().absolute() / "out",
-    help="Choose path to save ",
+    help="Path to save to",
 )
-@click.help_option("--help", "-h")
-def maze_generator(
-    grid_size: Size,
-    window_size: Size,
-    colour_speed: float,
-    start_colour: int,
-    frames_per_second: int,
-    steps_per_frame: int,
-    save_format: MediaFormat,
-    save_path: pathlib.Path,
-) -> None:
 
-    colour_speed = 1 / colour_speed
-    display.run(
-        grid_size=grid_size,
-        window_size=window_size,
-        colour_info=ColourInfo(start=start_colour, speed=colour_speed),
-        save_option=SaveOption(format=save_format, path=save_path),
-        speed=Speed(
-            frames_per_second=frames_per_second, steps_per_frame=steps_per_frame
-        ),
-    )
+DURATION_OPTION = click.option(
+    "--duration",
+    "-d",
+    "frame_duration",
+    default=100,
+    type=click.IntRange(1),
+    help="Duration between frames (ms)",
+)
+
+
+@click.group(invoke_without_command=True, context_settings={"max_content_width": 95})
+@add_options(MAZE_OPTIONS)
+@DURATION_OPTION
+@click.pass_context
+@click.help_option("--help", "-h")
+def maze(ctx, **kwargs):
+    if ctx.invoked_subcommand is None:
+        display.run(**kwargs)
+
+
+@maze.command()
+@add_options(MAZE_OPTIONS)
+@SAVE_PATH_OPTION
+@DURATION_OPTION
+def gif(**kwargs):
+    display.save_gif(**kwargs)
+
+
+@maze.command()
+@add_options(MAZE_OPTIONS)
+@SAVE_PATH_OPTION
+def png(save_path, **kwargs):
+    display.save_image(save_path=save_path.with_suffix(".png"), **kwargs)
+
+
+@maze.command()
+@add_options(MAZE_OPTIONS)
+@SAVE_PATH_OPTION
+def bmp(save_path, **kwargs):
+    display.save_image(save_path=save_path.with_suffix(".bmp"), **kwargs)
