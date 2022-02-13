8b88981d1be5430d3400b4dd9ced0672ba2b0448
wookie184 <wookie1840@gmail.com>
2022-02-13 16:35:10 +0000

    Remove unused constants and pull pyglet from master to fix bug with headless windows

--- maze/constants.py
+++ maze/constants.py
@@ -1,29 +1,9 @@
 from __future__ import annotations
 
 from enum import Enum, auto
-from pathlib import Path
-from typing import List, NamedTuple, Optional, Tuple
+from typing import NamedTuple, Tuple
 
 EdgeType = Tuple[int, int]
-
-
-class MediaFormat(Enum):
-    MP4 = auto()
-    GIF = auto()
-    PNG = auto()
-    JPEG = auto()
-    JPG = JPEG
-
-    @classmethod
-    def from_string(cls, string: str) -> MediaFormat:
-        try:
-            return cls.__members__[string]
-        except KeyError:
-            raise ValueError("Invalid MediaFormat string")
-
-    @classmethod
-    def formats(cls) -> List[str]:
-        return list(cls.__members__.keys())
 
 
 class ReturnType(Enum):
@@ -40,18 +20,3 @@ class Return(NamedTuple):
 class Size(NamedTuple):
     width: int
     height: int
-
-
-class ColourInfo(NamedTuple):
-    start: int
-    speed: float
-
-
-class SaveOption(NamedTuple):
-    format: MediaFormat
-    path: Optional[Path]
-
-
-class Speed(NamedTuple):
-    frames_per_second: int
-    steps_per_frame: int
